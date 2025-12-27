4b2cfc451ce3b0eb7280c3d205f48993d766cf04
bamlaku <abuyeget@gmail.com>
2025-12-27 14:04:34 +0300

    WP-8: Add LocationEnum for standardized origin/destination values

---
+++ src/domain/enums/location.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from enum import Enum
+
+class LocationEnum(Enum):
+    ADDIS_ABABA = "addis_ababa"
+    ADAMA = "adama"
+    DUKEM = "dukem"
+    DEBRE_ZEIT = "debre_zeit"
+    HAWASSA = "hawassa"
+    SHASHEMENE = "shashemene"
+    DJIBOUTI_PORT = "djibouti_port"
